1ec8a468e5f8352144a44b9609d0a2a109775533
PSW <sangwookp9591@gmail.com>
2021-01-11 21:10:52 +0900

    hello-spring start day-1

---
+++ src/test/java/hello/hellospring_2/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
