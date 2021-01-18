fbda8ffba3948f4dc837a8bc29a66c7e45db076c
PSW <sangwookp9591@gmail.com>
2021-01-18 21:33:11 +0900

    hello-spring start day-5 JDBC ,스프링 통합테스트, 스프링 JdbcTemplate

--- src/main/java/hello/hellospring_2/SpringConfig.java
+++ src/main/java/hello/hellospring_2/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hellospring_2;
 
+import hello.hellospring_2.repository.JdbcMemberRepository;
+import hello.hellospring_2.repository.JdbcTemplateMemberRepository;
 import hello.hellospring_2.repository.MemberRepository;
-import hello.hellospring_2.repository.MemoryMemberRepository;
 import hello.hellospring_2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean//Spring이 올라올때 얘를 spring bean contatiner에 올림
     public MemberService memberService(){
@@ -16,8 +28,9 @@ public class SpringConfig {
 
     @Bean//Spring이 올라올때 얘를 spring bean contatiner에 올림
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-
+        //return new MemoryMemberRepository();
+       // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 
